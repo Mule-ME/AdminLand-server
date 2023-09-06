a57037f83ad0ba0116ed4a54eca928d03143e82b
Mule-ME <mulualemesh15@gmail.com>
2023-09-06 21:34:49 +0300

    feat: get product with status implemented

--- controllers/client.js
+++ controllers/client.js
@@ -1,29 +1,20 @@
-import Product from "../models/Product.js"
-import ProductStat from "../models/ProductStat.js"
-
+import Product from "../models/Product.js";
 
 export const getProducts = async (req, res) => {
     try {
-
         const productWithStat = await Product.aggregate([
             {
                 $lookup: {
-                    'from': 'productstat',
-                    'localField': '_id',
-                    'foreignField': 'productId',
-                    'as': 'status'
-                }
+                    from: "productstats",
+                    localField: "_id",
+                    foreignField: "productId",
+                    as: "status",
+                },
             },
-
-
-        ])
-
-
+        ]);
 
         res.status(200).json(productWithStat);
-    }
-
-    catch (error) {
+    } catch (error) {
         res.status(404).json({ message: error.message });
     }
 };
